1a2de8e899abfff9327066b8b7a8b0ec345d036e
orelrafa <orelrafailov1998@gmail.com>
2024-09-18 23:16:44 +0300

    Half done, need to implement view details feature and deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+/* Useful shortcuts: CTRL+B to toggle the side panel
+ *                   CTRL+j to toggle the terminal
+ */
+
 import { useState } from "react";
 
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
@@ -6,13 +10,63 @@ import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
-    selectedProjectId: undefined,
+    selectedProjectId: undefined, //undefined will mean that we're doing nothing
     projects: [],
   });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null, //null will mean that we're adding a new project
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined, //null will mean that we're adding a new project
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProjectId = Math.round(Math.random * 1000);
+      const newProject = {
+        ...projectData,
+        id: newProjectId,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject
+        onAdd={handleAddProject}
+        onCancel={handleCancelAddProject}
+      ></NewProject>
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+    <main className="h-screen pt-8 flex gap-8">
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,15 +1,23 @@
 import Button from "./Button.jsx";
 
-export default function ProjectsSidebar() {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
-    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-tr-xl">
       <h2 className="mb-8 font-bold uppercase md:xl text-stone-200">
         Your Projects
       </h2>
       <div>
-        <Button>+ Add Project</Button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:stone-200">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
